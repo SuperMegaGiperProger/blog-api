efe259a2024675c7362d309662aaa9bf1988c005
Pavel Pershko <thrashmetalfan98@gmail.com>
2019-10-07 19:56:01 +0300

    Create posts table

---
+++ db/migrate/20191006175729_create_posts.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.references :user, foreign_key: true, null: false
+      t.string :title
+      t.string :body
+      t.inet :author_ip
+
+      t.timestamps
+    end
+  end
+end
