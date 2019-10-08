0785dba2155b281a0696768617bd005afa8337d3
Pavel Pershko <thrashmetalfan98@gmail.com>
2019-10-09 00:25:14 +0300

    Implement ratings controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
 class RatingsController < ApplicationController
-  def create; end
+  def create
+    rating = Rating.new rating_params
+
+    if rating.save
+      avg_rating = Post.find(rating.post_id).avg_rating
+
+      render json: { avg_rating: avg_rating }, status: :created
+    else
+      render json: { errors: rating.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def rating_params
+    params.permit(:post_id, :value)
+  end
 end
