c8fcdd1ff4693bd68dd6944aff9d2c105ff78412
Pavel Pershko <thrashmetalfan98@gmail.com>
2019-10-09 10:44:23 +0300

    Refactor repeated ips controller

--- app/controllers/repeated_ips_controller.rb
+++ app/controllers/repeated_ips_controller.rb
@@ -3,13 +3,13 @@
 class RepeatedIpsController < ApplicationController
   def index
     res = ActiveRecord::Base.connection.execute <<~SQL
-      SELECT DISTINCT login, pp.author_ip FROM
-        (SELECT COUNT(p.user_id), p.author_ip FROM
-          (SELECT DISTINCT user_id, author_ip FROM posts) as p
-        GROUP BY p.author_ip ORDER BY p.author_ip) as pp
-      LEFT JOIN posts ON posts.author_ip = pp.author_ip
+      SELECT DISTINCT users.login, ip_with_count.author_ip FROM
+        (SELECT COUNT(ip_history.user_id), ip_history.author_ip FROM
+          (SELECT DISTINCT user_id, author_ip FROM posts) ip_history
+        GROUP BY ip_history.author_ip ORDER BY ip_history.author_ip) ip_with_count
+      LEFT JOIN posts ON posts.author_ip = ip_with_count.author_ip
       LEFT JOIN users ON users.id = user_id
-      WHERE pp.count > 1 ORDER BY pp.author_ip
+      WHERE ip_with_count.count > 1 ORDER BY ip_with_count.author_ip
     SQL
 
     result = res.to_a.group_by { |row| row['author_ip'] }.transform_values { |val| val.map { |v| v['login'] } }
