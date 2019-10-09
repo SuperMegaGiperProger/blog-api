368862262ca1f5bf5c81b75bc78a2c32eb1a9fec
Pavel Pershko <thrashmetalfan98@gmail.com>
2019-10-09 03:07:38 +0300

    Implement repeated ips controller

--- app/controllers/repeated_ips_controller.rb
+++ app/controllers/repeated_ips_controller.rb
@@ -2,5 +2,18 @@
 
 class RepeatedIpsController < ApplicationController
   def index
+    res = ActiveRecord::Base.connection.execute <<~SQL
+      SELECT DISTINCT login, pp.author_ip FROM
+        (SELECT COUNT(p.user_id), p.author_ip FROM
+          (SELECT DISTINCT user_id, author_ip FROM posts) as p
+        GROUP BY p.author_ip ORDER BY p.author_ip) as pp
+      LEFT JOIN posts ON posts.author_ip = pp.author_ip
+      LEFT JOIN users ON users.id = user_id
+      WHERE pp.count > 1 ORDER BY pp.author_ip
+    SQL
+
+    result = res.to_a.group_by { |row| row['author_ip'] }.transform_values { |val| val.map { |v| v['login'] } }
+
+    render json: result
   end
 end
--- spec/controllers/repeated_ips_controller_spec.rb
+++ spec/controllers/repeated_ips_controller_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe RepeatedIpsController, type: :controller do
   describe 'GET index' do
     let(:uniq_ips) { ['12.13.14.15', '12.13.14.16', '12.13.14.17', '12.13.14.18', '12.13.14.19'] }
     let(:repeated_ips) do
-      [
-        { ip: '10.10.10.15', users: [users[0], users[2]] },
-        { ip: '10.10.10.16', users: [users[1], users[3], users[4], users[7]] },
-        { ip: '10.10.10.17', users: [users[0], users[1], users[6]] }
-      ]
+      {
+        '10.10.10.15': [users[0], users[2]],
+        '10.10.10.16': [users[1], users[3], users[4], users[7]],
+        '10.10.10.17': [users[0], users[1], users[6]]
+      }
     end
     let(:users) { create_list :user, 8 }
 
@@ -22,9 +22,9 @@ RSpec.describe RepeatedIpsController, type: :controller do
         2.times { create :post, user: user, author_ip: ip }
       end
 
-      repeated_ips.each do |data|
-        data[:users].each do |user|
-          create :post, user: user, author_ip: data[:ip]
+      repeated_ips.each do |ip, users|
+        users.each do |user|
+          create :post, user: user, author_ip: ip
         end
       end
     end
@@ -38,7 +38,7 @@ RSpec.describe RepeatedIpsController, type: :controller do
     end
 
     it 'returns ips, used by multiple users' do
-      result = repeated_ips.map { |data| { ip: data[:ip], logins: data[:users].map(&:login) } }
+      result = repeated_ips.transform_values { |users| users.map(&:login) }.stringify_keys
 
       expect(JSON.parse(response.body)).to match(result)
     end
